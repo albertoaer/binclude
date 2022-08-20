52ac92b14d95a218b084d24f605cb400bea701a8
Alberto Elorza <albertoelorza2002@gmail.com>
2022-08-20 18:57:16 +0200

    better cli

--- binclude/cli.py
+++ binclude/cli.py
@@ -1,14 +1,31 @@
-from typing import List
-from .utils import fatal, base_origin, origin
-from .builder import create
+import os
+from typing import Union
+from .utils import base_origin, join_paths, origin, valid_dir, write_into
+from .builder import create_for_python
+from .db import createDB, useDB
+
+BIN_NAME: str = 'binclude'
 
 class CLIController:
-    def init(self, folder: str):
-        payload = create(['python', origin()], 'python')
-        print(payload)
+    def init(self, dir: str):
+        if not valid_dir(dir):
+            raise Exception('Expecting valid directory')
+        db = createDB()
+        db.add_bin_dir(dir)
+        db.commit()
+        self.add(origin(), BIN_NAME, 'python', False, False)
 
-    def add(self, cmd: List[str], test: bool = False, hide: bool = False):
-        pass
+    def add(self, file: str, name: str, uses: Union[str, None] = None, test: bool = False, hide: bool = False):
+        db = useDB()
+        cmd = [file]
+        if uses:
+            cmd.insert(0, uses)
+        payload, interpreter = create_for_python(cmd, hide)
+        dir = db.get_bin_dir()
+        link = join_paths(dir, name)
+        db.add_link(name, cmd[len(cmd)-1], cmd[0], link, interpreter, 0)
+        write_into(payload, link)
+        db.commit()
 
     def remove(self, file: str):
         pass
@@ -16,5 +33,23 @@ class CLIController:
     def list(self):
         pass
 
+    def cwd(self):
+        print(os.getcwd())
+
     def whereis(self):
         print(base_origin())
+
+    def repldb(self):
+        """
+        REPL with the sqlite3 DB
+
+        Opens a REPL and executes the input in the database
+        In order to exit just input empty
+        """
+        db = useDB()
+        query: str = '.'
+        while query:
+            print('sqlite3 > ', end='')
+            query = input()
+            if query:
+                print(db.execute(query))
