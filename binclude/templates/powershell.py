3071cef6a623807274bfdd69a28b63a281faadb3
Alberto Elorza <albertoelorza2002@gmail.com>
2022-09-07 20:42:10 +0200

    empty line

--- binclude/templates/powershell.py
+++ binclude/templates/powershell.py
@@ -3,8 +3,7 @@ from typing import List, Set
 from .common import TemplateResult
 
 
-template = """
-& {target} $args
+template = """& {target} $args
 exit $LASTEXITCODE"""
 
 def build_template(target: List[str], attrs: Set[str]) -> TemplateResult:
